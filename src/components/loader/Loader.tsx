745d2d84aa9b253b97a57ecd35f9c1ea21ca0576
theo550 <theo.gaudin35@gmail.com>
2022-06-21 12:11:57 +0200

    add loader for low connexions

---
+++ src/components/loader/Loader.tsx
@@ -0,0 +1,11 @@
+import './loader.css'
+
+export default function Loader() {
+  return (
+    <div className="wrapper position-relative d-flex justify-content-around align-items-center mx-auto">
+      <div className="bg-success"></div>
+      <div className="bg-success"></div>
+      <div className="bg-success"></div>
+    </div>
+  )
+}
